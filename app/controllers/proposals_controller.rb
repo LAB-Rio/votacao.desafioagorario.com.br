b1ac3ad2ae71638aaad9490d317df38da180d293
Luiz Fonseca <eu@luiz.cc>
2015-05-04 18:02:04 -0300

    small design updates

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,7 +1,11 @@
 class ProposalsController < ApplicationController
+  respond_to :html
+
   def index
+    @proposals = Proposal.all
   end
 
   def show
+    @proposal = Proposal.find_by(id: params[:id])
   end
 end
