f0ff1e38735c8948b5de03f5fcb30df88d76c39a
Luiz Fonseca <eu@luiz.cc>
2015-05-05 19:33:48 -0300

    style improvements on the proposal's page.

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -5,4 +5,13 @@ class Proposal < ActiveRecord::Base
   def image
     !image_url.empty? ? image_url : 'http://i.imgur.com/WzBlGku.png'
   end
+
+
+  def next
+    where('id > ?', id).first || Proposal.first
+  end
+
+  def last
+    where('id < ?', id).last || Proposal.first
+  end
 end
