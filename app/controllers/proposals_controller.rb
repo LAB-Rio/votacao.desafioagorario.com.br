2cd68774103d1a42ba7db847ca99d0465102980c
Luiz Fonseca <eu@luiz.cc>
2015-05-05 21:09:33 -0300

    Add mandrill and randomness to proposals

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -2,7 +2,7 @@ class ProposalsController < ApplicationController
   respond_to :html
 
   def index
-    @proposals = Proposal.all
+    @proposals = Proposal.order('RANDOM()').all
   end
 
   def show
