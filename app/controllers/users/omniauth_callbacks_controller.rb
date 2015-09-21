4d57a97e5ac8b435a535cae6fac125e40b2e91ec
Luiz Fonseca <eu@luiz.cc>
2015-09-21 19:29:48 -0300

    Remove temporarily facebook login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to after_sign_in_path_for(resource)
+      redirect_to new_user_registration_url
     end
   end
 
