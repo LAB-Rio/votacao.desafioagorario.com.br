9abb0be50677a6f6b2944a499083979af03d3f9e
Luiz Fonseca <eu@luiz.cc>
2015-09-21 19:56:18 -0300

    Fix initial route.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   
   get '/confirm' => 'pages#confirmation', as: :confirmation_message
   get '/about' => 'pages#about', as: :about
-  root 'pages#index'
+  root 'proposals#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
