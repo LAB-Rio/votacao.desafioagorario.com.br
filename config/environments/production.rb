6d63205fee77aa99c1b9a927d3adc52f2ea68260
Luiz Fonseca <eu@luiz.cc>
2015-09-21 17:22:27 -0300

    WIP for the site.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
 
 
-  config.action_mailer.default_url_options = { :host => 'vote.conselhodajuventude.com.br' }
+  config.action_mailer.default_url_options = { :host => 'votacao.conselhodajuventude.com.br' }
   config.action_mailer.delivery_method = :smtp
  
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Votação Desafio Ágora Rio <contato@labrio.cc>'
+  config.mailer_sender = '[Votacao] Conselho da Juventude <contato@labrio.org>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
