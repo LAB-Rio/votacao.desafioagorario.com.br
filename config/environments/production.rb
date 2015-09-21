efc2f6bbb0e3a4d0565231dd8f8dc03f148213c4
Luiz Fonseca <eu@luiz.cc>
2015-09-21 16:54:46 -0300

    Use sendgrid as the main email server.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,17 +79,17 @@ Rails.application.configure do
 
 
 
-  config.action_mailer.default_url_options = { :host => 'vote.desafioagorario.com.br' }
+  config.action_mailer.default_url_options = { :host => 'vote.conselhodajuventude.com.br' }
   config.action_mailer.delivery_method = :smtp
  
 
 
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.mandrillapp.com',
+    :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['MANDRILL_USERNAME'],
-    :password       => ENV['MANDRILL_APIKEY'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
